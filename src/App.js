3dd96064d5a4d99a86c5ac3ff6be1382865a2be7
Sequeira, Ana <Ana.Sequeira@pmi.com>
2020-11-23 08:21:42 +0000

    component wish

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Wish from './Wish/Wish';
 class App extends Component {
+
+  state = {
+    name: 'mouse'
+
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+          <h1 className="App-title">Welcome to WishList Online</h1>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Wish name={this.state.name}/>
         </p>
       </div>
     );
---
+++ src/Wish/Wish.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const wish = (props) => {
+    return <div>
+        <p>Wish: {props.name}</p>
+    </div>
+}
+
+export default wish;

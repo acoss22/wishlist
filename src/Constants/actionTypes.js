9e4e8a25d3d720724fa591e1341713b5744ee426
Sequeira, Ana <Ana.Sequeira@pmi.com>
2020-11-23 12:18:34 +0000

    adding files for redux

---
+++ src/Constants/actionTypes.js
@@ -0,0 +1,9 @@
+export const ADD_ARTICLE = "ADD_WISHITEM";
+export const UPDATE_ARTICLE = "UPDATE_WISHITEM";
+export const SELECT_ARTICLE = "SELECT_WISHITEM";
+export const DELETE_ARTICLE = "DELETE_WISHITEM";
+
+export const OPEN_FORM = "OPEN_FORM";
+export const CLOSE_FORM = "CLOSE_FORM";
+export const OPEN_EDIT_FORM = "OPEN_EDIT_FORM";
+export const CLOSE_EDIT_FORM = "CLOSE_EDIT_FORM";

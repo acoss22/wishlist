e9db792cbcb104ab0021cc3014ceecadec565b76
Sequeira, Ana <Ana.Sequeira@pmi.com>
2020-11-23 11:51:52 +0000

    working crud wishes list

--- src/App.js
+++ src/App.js
@@ -1,32 +1,113 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Wish from './Wish/Wish';
+import { render } from 'react-dom';
+
+
+import WishItem from './Wish/WishItem'
+import AddWish from './Wish/AddWish'
+
+const wishes = [
+  {
+    name: 'iPad',
+    price: 200
+  },
+  {
+    name: 'iPhone',
+    price: 650
+  }
+]
+
+localStorage.setItem('wishes', JSON.stringify(wishes))
+
 class App extends Component {
 
-  state = {
-    wishes : [
-      {wishname: 'new mouse'},
-      {wishname: 'new keyboard'},
-      {wishname: 'new headset'}
-    ]
-    
+  constructor(props){
+    super(props)
 
+    this.state = {
+      wishes: JSON.parse(localStorage.getItem('wishes'))
+    }
+
+    this.onAdd = this.onAdd.bind(this);
+    this.onDelete = this.onDelete.bind(this);
+    this.onEditSubmit = this.onEditSubmit.bind(this);
   }
 
+  componentWillMount(){
+    const wishes = this.getwishes()
+
+    this.setState({wishes})
+  }
+
+  getwishes(){
+    return this.state.wishes
+  }
+
+/////////////////////////
+
+  onAdd(name, price){
+    const wishes = this.getwishes()
+
+    wishes.push({
+      name,
+      price
+    })
+
+    this.setState({wishes})
+  }
+
+  onDelete(name){
+    const wishes = this.getwishes()
+
+    const filteredwishes = wishes.filter(product => {
+      return product.name !== name
+    })
+
+    this.setState({wishes:filteredwishes})
+  }
+
+  onEditSubmit(name, price, originalName){
+    let wishes = this.getwishes();
+
+    wishes = wishes.map(product => {
+      if(product.name === originalName){
+          product.name = name
+          product.price = price
+      }
+
+      return product
+    })
+
+    this.setState({wishes})
+  }
+
+/////////////////////////
 
   render() {
+
     return (
-      <div className="App">
-          <h1 className="App-title">Welcome to WishList Online</h1>
-        <p className="App-intro">
-          <Wish wishname={this.state.wishes[0].wishname}/>
-          <Wish wishname={this.state.wishes[1].wishname}/>
-          <Wish wishname={this.state.wishes[2].wishname}/>
-        </p>
+      <div>
+        <h1>Wishlist Online</h1>
+
+        <AddWish 
+          onAdd={this.onAdd}
+        />
+
+        {
+          this.state.wishes.map(product =>{
+            return(
+              <WishItem
+                key={product.name}
+                {...product}
+                onDelete={this.onDelete}
+                onEditSubmit={this.onEditSubmit}
+              />
+            )
+          })
+        }
       </div>
-    );
+      )
   }
 }
 
+render(<App />, document.getElementById('root'));
 export default App;
--- src/Wish/Wish.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const wish = (props) => {
-    return <div>
-        <p>Wish: {props.wishname}</p>
-    </div>
-}
-
-export default wish;

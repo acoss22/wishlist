b368fe98aa6fe1b496f7ef1860dda82d9dcee46e
Sequeira, Ana <Ana.Sequeira@pmi.com>
2020-11-23 11:59:21 +0000

    added comments

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-
-
 import WishItem from './Wish/WishItem'
 import AddWish from './Wish/AddWish'
 
+//1st initial state declared
 const wishes = [
   {
     name: 'iPad',
@@ -16,34 +15,38 @@ const wishes = [
   }
 ]
 
+//saved to localstorage because not using redux yet
 localStorage.setItem('wishes', JSON.stringify(wishes))
 
 class App extends Component {
-
+//needs contructor and super to pass props
   constructor(props){
     super(props)
 
+      //state is updated from localstorage 
     this.state = {
       wishes: JSON.parse(localStorage.getItem('wishes'))
     }
 
+    //binding methods to this state
     this.onAdd = this.onAdd.bind(this);
     this.onDelete = this.onDelete.bind(this);
     this.onEditSubmit = this.onEditSubmit.bind(this);
   }
 
+    //hook gets the snapshop before the update
   componentWillMount(){
     const wishes = this.getwishes()
 
     this.setState({wishes})
   }
-
+  //gets the state from tis
   getwishes(){
     return this.state.wishes
   }
 
 /////////////////////////
-
+//gets the state and pushes it 
   onAdd(name, price){
     const wishes = this.getwishes()
 

21cde46090060f64af01011128a89b6c602ec3d1
acoss22 <anacseq@gmail.com>
2020-12-06 08:24:36 +0000

    router added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import { Wishlist } from './features/wishlist/Wishlist';
 import './App.css';
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Contact from "./Contact/Contact";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Wishlist />
-      </header>
+        <div className="container">
+        <Router>
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route path="/Contact" component={Contact} />
+
+            
+              <Wishlist />
+              <div className="list"></div>
+            </Switch>
+          </div>
+          <Footer />
+        </Router>
+      </div>
     </div>
   );
 }
